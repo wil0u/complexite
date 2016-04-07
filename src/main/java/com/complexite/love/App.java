36ba4f7d8624f7caaa005f3a7ac2c3b539b63ff7
wil0u <willeme.verdeaux@gmail.com>
2016-04-07 17:14:48 +0200

    base du projet

---
+++ src/main/java/com/complexite/love/App.java
@@ -0,0 +1,13 @@
+package com.complexite.love;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
