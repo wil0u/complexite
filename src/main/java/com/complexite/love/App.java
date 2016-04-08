916d24ad487be08315fc164bac9b853e0dec3e71
Pierre RUDELOU <pierre.rudelou@hotmail.fr>
2016-04-08 17:08:26 +0200

    Fenetre FAIT Interface Graphique onglet calcul FAIT
    https://www.youtube.com/watch?v=EOxUWLl2HFs

--- src/main/java/com/complexite/love/App.java
+++ src/main/java/com/complexite/love/App.java
@@ -1,13 +1,119 @@
 package com.complexite.love;
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import javax.swing.JScrollPane;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
+	
+	static Color colorBtnBlue = new Color (51,102, 204);
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+/**==================================================== Edition generale de la fenetre =================================================**/
+    		// Création de la variable fenetre
+    		JFrame fenetre = new JFrame();
+    		
+    	    //Titre de la fenetre
+    	    fenetre.setTitle("Coloration de Graphe");
+    	    
+    	    // Taille de la fenetre
+    	    fenetre.setSize(700, 400);
+    	    
+    	    //Positionnement de la fenetre au centre
+    	    fenetre.setLocationRelativeTo(null);
+    	    
+    	    // Croix rouge de la fenetre effective
+    	    fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	    
+    	    // Permet à la fenetre d'accepter les onglets
+    	    JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+    	    fenetre.getContentPane().add(tabbedPane, BorderLayout.CENTER);
+
+ /**==================================================== Edition fenetre Calcul ========================================================**/    	    
+    	    //Création de l'onglet fenetreCalcul
+    	    JPanel fenetreCalcul = new JPanel();
+    	    tabbedPane.addTab("Calcul", null, fenetreCalcul, null);
+    	    fenetreCalcul.setLayout(new BorderLayout(0, 0));
+    	    
+    	    // Ajout de la partie permettant de selectionner Nb Noeud en haut de la fenetre
+    	    JPanel panelNbNoeud = new JPanel();
+    	    FlowLayout flowLayout = (FlowLayout) panelNbNoeud.getLayout();
+    	    flowLayout.setAlignment(FlowLayout.LEFT);
+    	    fenetreCalcul.add(panelNbNoeud, BorderLayout.NORTH);
+    	    
+    	    JLabel lblNbNoeud = new JLabel("          Nb Noeud : ");
+    	    panelNbNoeud.add(lblNbNoeud);
+    	    
+    	    JTextField textNbNoeud = new JTextField();
+    	    panelNbNoeud.add(textNbNoeud);
+    	    textNbNoeud.setColumns(5);
+    	    
+    	    JButton btnNbNoeud = new JButton("Valider");
+    	    panelNbNoeud.add(btnNbNoeud);
+    	    
+    	    
+    	    // Panel permettant de faire un vide sur la gauche
+    	    JPanel panelVideGauche = new JPanel();
+    	    fenetreCalcul.add(panelVideGauche, BorderLayout.WEST); 	    
+    	    JLabel lblVideGauche = new JLabel("                               ");
+    	    panelVideGauche.add(lblVideGauche);
+    	    
+    	    
+    	    // Panel permettant de faire un vide sur la droite
+    	    JPanel panelVideDroite = new JPanel();
+    	    fenetreCalcul.add(panelVideDroite, BorderLayout.EAST);    	    
+    	    JLabel lblVideDroite = new JLabel("                               ");
+    	    panelVideDroite.add(lblVideDroite);
+    	    
+    	    
+    	    // Panel de lancement se trouvant en bas de la fenêtre
+    	    JPanel panelConfirmer = new JPanel();
+    	    fenetreCalcul.add(panelConfirmer, BorderLayout.SOUTH);
+    	    
+    	    JLabel lblConfirmerLancement = new JLabel("Lancement               ");
+    	    panelConfirmer.add(lblConfirmerLancement);
+    	    
+    	    JButton btnConfirmerBSC = new JButton("     BSC     ");
+    	    panelConfirmer.add(btnConfirmerBSC);
+    	    
+    	    JLabel lblConfirmerVide01 = new JLabel("               ");
+    	    panelConfirmer.add(lblConfirmerVide01);
+    	    
+    	    JButton btnConfirmerDSATUR = new JButton(" DSATUR ");
+    	    panelConfirmer.add(btnConfirmerDSATUR);
+    	    
+    	    JLabel lblConfirmerVide02 = new JLabel("               ");
+    	    panelConfirmer.add(lblConfirmerVide02);
+    	    
+    	    JButton btnConfirmerTABUCOL = new JButton("TABUCOL");
+    	    panelConfirmer.add(btnConfirmerTABUCOL);
+    	    
+    	    JScrollPane scrollPane = new JScrollPane();
+    	    fenetreCalcul.add(scrollPane, BorderLayout.CENTER);
+    	    
+    	    
+/**==================================================== Edition fenetre Graphe ========================================================**/
+    	    
+    	  //Création de l'onglet fenetreGraphe
+    	    JPanel fenetreGraphe = new JPanel();
+    	    tabbedPane.addTab("Graphe", null, fenetreGraphe, null);
+    		
+
+    	    
+    		// Affichage de la fenêtre
+    		fenetre.setVisible(true);
     }
 }
+
